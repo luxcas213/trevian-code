213a9e92dc007c57e0b44624f5934e1706d22871
Ignacio Pardo <65306107+IgnacioPardo@users.noreply.github.com>
2025-07-15 08:08:32 -0300

    Initial Commit

---
+++ trevian/trevianApp.swift
@@ -0,0 +1,17 @@
+//
+//  trevianApp.swift
+//  trevian
+//
+//  Created by TIC on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct trevianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
